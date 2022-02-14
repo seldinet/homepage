6285ea28fc0af8101d9bdd13e1c64327f2770e5c
DeokHyeon,Son <crew0813@gmail.com>
2022-02-14 22:19:29 +0900

    fix: company right name

--- src/i18n/locales/zh/footer.ts
+++ src/i18n/locales/zh/footer.ts
@@ -2,7 +2,7 @@ export default {
   mode: "zh",
   footer: {
     seldiInformation:
-      "（株）YOUYISHOU KOREA l首尔市江南大路364MEEWANG大厦1709 l 代表：李昌宪 l 营业执照号：568-88-02012",
+      "（株）Youyishou Co,LTD. l首尔市江南大路364MEEWANG大厦1709 l 代表：李昌宪 l 营业执照号：568-88-02012",
     seldiNumber: "电话：02-6958-8556 l 传真：0504-051-2750",
     consultingTitle: "咨询电话",
     consultingTime: `平日：09:00~18:00(午休:11:30~12:30)
