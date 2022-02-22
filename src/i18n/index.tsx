125c8c8a30ca68af1cbdfb78288d500e7b325b2f
DeokHyeon,Son <crew0813@gmail.com>
2022-02-22 14:18:52 +0900

    fix: build file

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -1,10 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 import { kr, zh } from "./locales";
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
